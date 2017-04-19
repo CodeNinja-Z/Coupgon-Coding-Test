448afea7e308c659d2ac5aee01e53b613f46c61c
Carlos Zhao <blacklight728@gmail.com>
2017-04-18 22:48:34 -0400

    Add routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  resources :golf_clubs, only: [:show, :edit, :update] do
+    resources :reservations, only: [:index, :create, :edit, :update, :destroy]
+  end
+
+  get '/owner_index', to: 'golf_clubs#owner_index', as: 'owner_index'
+
+  root 'golf_clubs#show'
 end
